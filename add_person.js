c759c835547c2d106d0a53edaa2201427305c4d0
Dilan <dilannebioglu@gmail.com>
2017-10-24 20:55:05 +0000

    initialized migration

--- add_person.js
+++ add_person.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'development',
-    password : 'development',
-    database : 'vagrant'
+    host: '127.0.0.1',
+    user: 'development',
+    password: 'development',
+    database: 'vagrant'
   }
 });
 
@@ -14,11 +14,15 @@ let input = process.argv.slice(2, 5);
 
 knex('famous_people')
   .returning('id')
-  .insert({first_name: input[0], last_name: input[1], birthdate: input[2]})
+  .insert({
+    first_name: input[0],
+    last_name: input[1],
+    birthdate: input[2]
+  })
   .asCallback((err, rows) => {
     if (err) throw err;
     rows.forEach(function(e) {
-    console.log('inserted');
+      console.log('inserted');
     })
   })
   .finally(() => knex.destroy())
--- lookup_people_knex.js
+++ lookup_people_knex.js
@@ -1,10 +1,10 @@
 var knex = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'development',
-    password : 'development',
-    database : 'vagrant'
+    host: '127.0.0.1',
+    user: 'development',
+    password: 'development',
+    database: 'vagrant'
   }
 });
 
@@ -13,14 +13,14 @@ var knex = require('knex')({
 let input = process.argv[2];
 
 knex.select().from('famous_people')
-.where('first_name', 'like', `${input}`)
-.orWhere('last_name', 'like', `${input}`)
-.asCallback((err, rows) => {
-  if (err) throw err;
-  console.log('Searching...');
-  console.log(`Found 1 person(s) associated with your query: '${input}'`);
-  rows.forEach(function(e) {
-  console.log("- " + e.id + ": " + e.first_name + " " + e.last_name + ", born " + e.birthdate);
-})
-})
-.then(() => knex.destroy())
+  .where('first_name', 'like', `${input}`)
+  .orWhere('last_name', 'like', `${input}`)
+  .asCallback((err, rows) => {
+    if (err) throw err;
+    console.log('Searching...');
+    console.log(`Found 1 person(s) associated with your query: '${input}'`);
+    rows.forEach(function(e) {
+      console.log("- " + e.id + ": " + e.first_name + " " + e.last_name + ", born " + e.birthdate);
+    })
+  })
+  .then(() => knex.destroy())
